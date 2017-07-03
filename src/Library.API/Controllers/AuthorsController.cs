e9ff8b1251db0d1337eef83ba25dda617c90cdb2
dostap <dig14step@gmail.com>
2017-07-03 13:03:22 -0600

    added authors controller

---
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,23 @@
+﻿using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private ILibraryRepository _libraryRepository;
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
